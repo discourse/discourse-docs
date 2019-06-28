6add4cfc5f776e95a2089c340e60998b2230a98c
Justin DiRose <justin@justindirose.com>
2019-06-28 11:27:39 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: discourse-knowledge-base
+# about: A plugin to make it easy to find knowledge base articles in Discourse
+# version: 0.1
+# author: Justin DiRose
+# license: 
+# url: 
+
+enabled_site_setting :knowledge_base_enabled
