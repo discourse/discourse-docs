1b81e0e91f6ada53ff62df5aa0c1567f9272b730
Justin DiRose <justin@justindirose.com>
2019-10-08 14:49:54 -0500

    Debug commit

--- lib/knowledge_explorer/query.rb
+++ lib/knowledge_explorer/query.rb
@@ -9,32 +9,50 @@ module KnowledgeExplorer
     end
 
     def categories
-      SiteSetting.knowledge_explorer_categories.split("|")
+      SiteSetting.knowledge_explorer_categories.split('|')
     end
 
     def tags
-      SiteSetting.knowledge_explorer_tags.split("|")
+      SiteSetting.knowledge_explorer_tags.split('|')
     end
 
     def get
 
       # query for topics matching selected categories & tags
       tq = TopicQuery.new(@user)
-      results = tq.latest_results({:no_definitions => true})
+      results = tq.latest_results({ :no_definitions => true })
       results = results.left_outer_joins(:tags)
       results = results.where('category_id IN (?)', categories).or(results.where('tags.name IN (?)', tags))
 
+      # filter results by selected category
+      unless @filters[:category].nil?
+        results = results.where('category_id IN (?)', @filters[:category])
+      end
+
       # filter results by selected tags
-      if !@filters[:tags].nil?
-        results = results.where('tags.name IN (?)', @filters[:tags])
+      unless @filters[:tags].nil?
+        tag_filters = @filters[:tags].split('|')
+        results = results.where('tags.name IN (?)', tag_filters)
+          .group('id')
+          .having('count(distinct tags.name) = ?', tag_filters.count)
       end
 
       # filter results by search term
-      if !@filters[:search_term].nil?
+      unless @filters[:search_term].nil?
         results = results.where('title LIKE ?', "%#{@filters[:search_term]}%")
       end
 
-      # get tag count
+      tags = tag_count(results)
+
+      # assemble the object
+      topic_query = tq.create_list(:knowledge_explorer, {}, results)
+
+      topic_list = TopicListSerializer.new(topic_query, scope: Guardian.new(@user)).as_json
+
+      { tags: tags, topics: topic_list }
+    end
+
+    def tag_count(results)
       tags = []
 
       results.each do |topic|
@@ -51,14 +69,7 @@ module KnowledgeExplorer
         end
       end
 
-      tags = tags.sort_by { |tag| [tag[:active] ? 0 : 1, -tag[:count]] }
-
-      # assemble the object
-      topic_query = tq.create_list(:knowledge_explorer, {}, results)
-
-      topic_list = TopicListSerializer.new(topic_query, scope: Guardian.new(@user)).as_json
-
-      { tags: tags, topics: topic_list }
+      tags.sort_by { |tag| [tag[:active] ? 0 : 1, -tag[:count]] }
     end
   end
 end
