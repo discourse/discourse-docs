207ee6f0f7c102038c7b063359c40e63081dd9c4
Joshua Rosenfeld <jomaxro@discourse.org>
2020-05-12 16:13:51 -0400

    Add plugin URL

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,7 @@
 # about: A plugin to make it easy to explore and find knowledge base-type articles in Discourse
 # version: 0.1
 # author: Justin DiRose
+# url: https://github.com/discourse/discourse-knowledge-explorer
 
 enabled_site_setting :knowledge_explorer_enabled
 
