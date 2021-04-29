discourse/discourse-docs

commit f75c9d5e3276dc26bac8cdcd5b2dd75eb6ce1614

DEV: Update CI workflows, fix linting issues (#32)
Included:

* DEV: Update CI workflows
* DEV: Update the plugin URL
* DEV: Update Gemfile.lock
* DEV: Remove an old rubocop artifact
* DEV: Add .prettierrc
* DEV: Clean up .gitignore
* DEV: Add license
* DEV: Update package.json
* DEV: Fix template issues
* DEV: Re-format code
* DEV: Use `@action`
* DEV: `inject as controller`
* DEV: Use `@computed`
* DEV: Avoid naming collision and extra indirection
* DEV: Use `discourse-common/utils/decorators`
* DEV: Use `role="button"`

Co-authored-by: Jarek Radosz <jradosz@gmail.com>
Co-authored-by: jjaffeux <j.jaffeux@gmail.com>

assets/javascripts/discourse/models/docs.js.es6
@@ -7,6 +7,7 @@ const Docs = EmberObject.extend({});
 Docs.reopenClass({
   list(params) {
     let filters = [];
+
     if (params.filterCategories) {
       filters.push(`category=${params.filterCategories}`);
     }
@@ -53,10 +54,7 @@ Docs.reopenClass({
 
     promise = promise.then((data) => {
       data.topics.topic_list.topics = data.topics.topic_list.topics.map(
-        (topic) => {
-          topic = Topic.create(topic);
-          return topic;
-        }
+        (topic) => Topic.create(topic)
       );
       return data;
     });

assets/javascripts/discourse/templates/components/docs-category.hbs
@@ -1,10 +1,12 @@
-<a  {{action "selectCategory"}} class="docs-item docs-category {{if category.active 'selected'}}">
+<a href {{action "selectCategory"}} class="docs-item docs-category {{if category.active "selected"}}">
   {{#unless category.active}}
     {{d-icon "plus"}}
   {{/unless}}
+
   {{#if category.active}}
     {{d-icon "times-circle"}}
   {{/if}}
+
   <span class="docs-item-id category-id">{{categoryName}}</span>
   <span class="docs-item-count category-count">{{category.count}}</span>
 </a>

assets/javascripts/discourse/templates/components/docs-tag.hbs
@@ -1,10 +1,12 @@
-<a {{action "selectTag"}} class="docs-item docs-tag {{if tag.active 'selected'}} {{if subtag 'subtag'}}">
+<a href {{action "selectTag"}} class="docs-item docs-tag {{if tag.active "selected"}} {{if subtag "subtag"}}">
   {{#unless tag.active}}
     {{d-icon "plus"}}
   {{/unless}}
+
   {{#if tag.active}}
     {{d-icon "times-circle"}}
   {{/if}}
+
   <span class="docs-item-id tag-id">{{tag.id}}</span>
   <span class="docs-item-count tag-count">{{tag.count}}</span>
 </a>

assets/javascripts/discourse/templates/docs-index.hbs
@@ -1,15 +1,16 @@
 {{#conditional-loading-spinner condition=isLoading}}
   {{#if emptyTopics}}
-    <span class="no-topics-found">{{html-safe (i18n 'docs.no_topics')}}</span>
+    <span class="no-topics-found">{{html-safe (i18n "docs.no_topics")}}</span>
   {{else}}
-  <div class="docs-browse">
-    {{#if site.mobileView}}
-      {{#unless selectedTopic}}
-        {{d-button class="docs-expander" icon=(if expandedFilters "angle-up" "angle-down") action=(action "toggleFilters") label="docs.filter_button"}}
-      {{/unless}}
-    {{/if}}
-    <div class="docs-filters">
-      {{#if expandedFilters}}
+    <div class="docs-browse">
+      {{#if site.mobileView}}
+        {{#unless selectedTopic}}
+          {{d-button class="docs-expander" icon=(if expandedFilters "angle-up" "angle-down") action=(action "toggleFilters") label="docs.filter_button"}}
+        {{/unless}}
+      {{/if}}
+
+      <div class="docs-filters">
+        {{#if expandedFilters}}
           {{#if canFilterSolved}}
             <div class="docs-items docs-solved">
               <label class="checkbox-label docs-item">
@@ -21,11 +22,11 @@
                 {{i18n "docs.filter_solved"}}
               </label>
             </div>
-
           {{/if}}
+
           {{#if categories}}
             <div class="docs-items docs-categories">
-              <h3>{{i18n 'docs.categories'}}</h3>
+              <h3>{{i18n "docs.categories"}}</h3>
               {{#each categories as |category|}}
                 {{docs-category
                   category=category
@@ -35,9 +36,10 @@
               {{/each}}
             </div>
           {{/if}}
+
           {{#if tags}}
             <div class="docs-items docs-tags">
-              <h3>{{i18n 'docs.tags'}}</h3>
+              <h3>{{i18n "docs.tags"}}</h3>
               {{#each tags as |tag|}}
                 {{docs-tag
                   tag=tag
@@ -46,41 +48,41 @@
               {{/each}}
             </div>
           {{/if}}
+        {{/if}}
+      </div>
+
+      {{#if selectedTopic}}
+        {{#conditional-loading-spinner condition=isTopicLoading}}
+          {{docs-topic topic=topic return=(action "returnToList")}}
+        {{/conditional-loading-spinner}}
+      {{else}}
+        <div class="docs-results">
+          {{#if isSearchingOrFiltered}}
+            {{#if emptyResults}}
+              <div class="result-count no-result">
+                {{i18n "search.no_results"}}
+              </div>
+              {{plugin-outlet name="after-docs-empty-results"}}
+            {{else}}
+              <div class="result-count">
+                {{i18n "docs.search.results" count=topicCount}}
+              </div>
+            {{/if}}
+          {{/if}}
+
+          {{#unless emptyResults}}
+            {{docs-topic-list
+              topics=topics
+              ascending=ascending
+              order=orderColumn
+              sortBy=(action "sortBy")
+              selectTopic=(action "setSelectedTopic")
+              loadMore=(action "loadMore")
+              loading=isLoadingMore
+            }}
+          {{/unless}}
+        </div>
       {{/if}}
     </div>
-    {{#if selectedTopic}}
-      {{#conditional-loading-spinner condition=isTopicLoading}}
-        {{docs-topic topic=topic return=(action "returnToList")}}
-      {{/conditional-loading-spinner}}
-    {{else}}
-      <div class="docs-results">
-        {{#if isSearchingOrFiltered}}
-          {{#if emptyResults}}
-            <div class="result-count no-result">
-              {{i18n 'search.no_results'}}
-            </div>
-            {{plugin-outlet name="after-docs-empty-results"}}
-          {{else}}
-            <div class="result-count">
-              {{i18n 'docs.search.results'
-                count=topicCount
-              }}
-            </div>
-          {{/if}}
-        {{/if}}
-        {{#unless emptyResults}}
-          {{docs-topic-list
-            topics=topics
-            ascending=ascending
-            order=orderColumn
-            sortBy=(action "sortBy")
-            selectTopic=(action "setSelectedTopic")
-            loadMore=(action "loadMore")
-            loading=isLoadingMore
-          }}
-        {{/unless}}
-      </div>
-    {{/if}}
-  </div>
   {{/if}}
 {{/conditional-loading-spinner}}

assets/javascripts/discourse/controllers/docs.js.es6
@@ -1,19 +1,24 @@
-import Controller, { inject } from "@ember/controller";
+import Controller, { inject as controller } from "@ember/controller";
+import { action } from "@ember/object";
 
 export default Controller.extend({
-  indexController: inject("docs.index"),
-  actions: {
-    updateSelectedCategories(category) {
-      this.indexController.send("updateSelectedCategories", category);
-      return false;
-    },
-    updateSelectedTags(tag) {
-      this.indexController.send("updateSelectedTags", tag);
-      return false;
-    },
-    performSearch(term) {
-      this.indexController.send("performSearch", term);
-      return false;
-    },
+  indexController: controller("docs.index"),
+
+  @action
+  updateSelectedCategories(category) {
+    this.indexController.send("updateSelectedCategories", category);
+    return false;
+  },
+
+  @action
+  updateSelectedTags(tag) {
+    this.indexController.send("updateSelectedTags", tag);
+    return false;
+  },
+
+  @action
+  performSearch(term) {
+    this.indexController.send("performSearch", term);
+    return false;
   },
 });

.github/workflows/plugin-linting.yml
@@ -20,18 +20,13 @@ jobs:
           node-version: 12
 
       - name: Set up ruby
-        uses: actions/setup-ruby@v1
+        uses: ruby/setup-ruby@v1
         with:
           ruby-version: 2.7
-
-      - name: Setup bundler
-        run: gem install bundler -v 2.1.4 --no-doc
-
-      - name: Setup gems
-        run: bundle install --jobs 4
+          bundler-cache: true
 
       - name: Yarn install
-        run: yarn install --dev
+        run: yarn install
 
       - name: ESLint
         run: yarn eslint --ext .js,.js.es6 --no-error-on-unmatched-pattern {test,assets}/javascripts
@@ -45,6 +40,9 @@ jobs:
           if [ -d "test" ]; then \
             yarn prettier --list-different "test/**/*.{js,es6}" ; \
           fi
+
+      - name: Ember template lint
+        run: yarn ember-template-lint assets/javascripts
 
       - name: Rubocop
         run: bundle exec rubocop .

.github/workflows/plugin-tests.yml
@@ -10,14 +10,15 @@ on:
 jobs:
   build:
     name: ${{ matrix.build_type }}
-    runs-on: ${{ matrix.os }}
+    runs-on: ubuntu-latest
+    container: discourse/discourse_test:release
     timeout-minutes: 60
 
     env:
       DISCOURSE_HOSTNAME: www.example.com
       RUBY_GLOBAL_METHOD_CACHE_SIZE: 131072
       RAILS_ENV: test
-      PGHOST: localhost
+      PGHOST: postgres
       PGUSER: discourse
       PGPASSWORD: discourse
 
@@ -26,8 +27,7 @@ jobs:
 
       matrix:
         build_type: ["backend", "frontend"]
-        os: [ubuntu-latest]
-        ruby: ["2.6"]
+        ruby: ["2.7"]
         postgres: ["12"]
         redis: ["4.x"]
 
@@ -47,13 +47,13 @@ jobs:
           --health-retries 5
 
     steps:
-      - uses: actions/checkout@master
+      - uses: actions/checkout@v2
         with:
           repository: discourse/discourse
           fetch-depth: 1
 
       - name: Install plugin
-        uses: actions/checkout@master
+        uses: actions/checkout@v2
         with:
           path: plugins/${{ github.event.repository.name }}
           fetch-depth: 1
@@ -75,46 +75,30 @@ jobs:
           git config --global user.email "ci@ci.invalid"
           git config --global user.name "Discourse CI"
 
-      - name: Setup packages
-        run: |
-          sudo apt-get update
-          sudo apt-get -yqq install postgresql-client libpq-dev gifsicle jpegoptim optipng jhead
-          wget -qO- https://raw.githubusercontent.com/discourse/discourse_docker/master/image/base/install-pngquant | sudo sh
-
-      - name: Update imagemagick
-        if: matrix.build_type == 'backend'
-        run: |
-          wget https://raw.githubusercontent.com/discourse/discourse_docker/master/image/base/install-imagemagick
-          chmod +x install-imagemagick
-          sudo ./install-imagemagick
-
       - name: Setup redis
         uses: shogo82148/actions-setup-redis@v1
         with:
           redis-version: ${{ matrix.redis }}
 
-      - name: Setup ruby
-        uses: actions/setup-ruby@v1
-        with:
-          ruby-version: ${{ matrix.ruby }}
-
-      - name: Setup bundler
-        run: |
-          gem install bundler -v 2.1.4 --no-doc
-          bundle config deployment 'true'
-          bundle config without 'development'
-
       - name: Bundler cache
         uses: actions/cache@v2
-        id: bundler-cache
         with:
           path: vendor/bundle
-          key: ${{ runner.os }}-gem-${{ hashFiles('**/Gemfile.lock') }}
+          key: ${{ runner.os }}-${{ matrix.ruby }}-gem-${{ hashFiles('**/Gemfile.lock') }}
           restore-keys: |
-            ${{ runner.os }}-gem-
+            ${{ runner.os }}-${{ matrix.ruby }}-gem-
 
       - name: Setup gems
-        run: bundle install --jobs 4
+        run: |
+          bundle config --local path vendor/bundle
+          bundle config --local deployment true
+          bundle config --local without development
+          bundle install --jobs 4
+          bundle clean
+
+      - name: Lint English locale
+        if: matrix.build_type == 'backend'
+        run: bundle exec ruby script/i18n_lint.rb "plugins/${{ github.event.repository.name }}/locales/{client,server}.en.yml"
 
       - name: Get yarn cache directory
         id: yarn-cache-dir
@@ -130,7 +114,7 @@ jobs:
             ${{ runner.os }}-${{ matrix.os }}-yarn-
 
       - name: Yarn install
-        run: yarn install --dev
+        run: yarn install
 
       - name: Migrate database
         run: |

.gitignore
@@ -1,4 +1,1 @@
- .rubocop-https---raw-githubusercontent-com-discourse-discourse-master--rubocop-yml
 node_modules
-yarn-error.log
-.rubocop-https---raw-githubusercontent-com-discourse-*

commit 378f8d24f40dd8b1cbfa7d0d5ff2ca5b0fe7793e

FIX: Correctly trigger actions (#45)
Fixes a regression introduced in #32. Reported in https://meta.discourse.org/t/meta-discourse-org-docs-sidebar-filter-doesnt-work/188350/2

Included:
* DEV: Use `query` helper
* DEV: Simplify the Docs model
* DEV: Add a test
* FIX: Correctly trigger actions
* DEV: Use `count` instead of `queryAll`

assets/javascripts/discourse/models/docs.js.es6
@@ -33,33 +33,22 @@ Docs.reopenClass({
       filters.push(`topic=${params.selectedTopic}`);
     }
 
-    let promise = ajax(`/docs.json?${filters.join("&")}`);
-
-    promise = promise.then((data) => {
+    return ajax(`/docs.json?${filters.join("&")}`).then((data) => {
       data.topics.topic_list.topics = data.topics.topic_list.topics.map(
-        (topic) => {
-          topic = Topic.create(topic);
-          return topic;
-        }
+        (topic) => Topic.create(topic)
       );
       data.topic = Topic.create(data.topic);
       return data;
     });
-
-    return promise;
   },
 
   loadMore(loadMoreUrl) {
-    let promise = ajax(loadMoreUrl);
-
-    promise = promise.then((data) => {
+    return ajax(loadMoreUrl).then((data) => {
       data.topics.topic_list.topics = data.topics.topic_list.topics.map(
         (topic) => Topic.create(topic)
       );
       return data;
     });
-
-    return promise;
   },
 });
 

assets/javascripts/discourse/templates/components/docs-category.hbs
@@ -1,4 +1,4 @@
-<a href {{action "selectCategory"}} class="docs-item docs-category {{if category.active "selected"}}">
+<a href {{action this.selectCategory}} class="docs-item docs-category {{if category.active "selected"}}">
   {{#unless category.active}}
     {{d-icon "plus"}}
   {{/unless}}

assets/javascripts/discourse/templates/components/docs-tag.hbs
@@ -1,4 +1,4 @@
-<a href {{action "selectTag"}} class="docs-item docs-tag {{if tag.active "selected"}} {{if subtag "subtag"}}">
+<a href {{action this.selectTag}} class="docs-item docs-tag {{if tag.active "selected"}} {{if subtag "subtag"}}">
   {{#unless tag.active}}
     {{d-icon "plus"}}
   {{/unless}}

assets/javascripts/discourse/templates/docs-index.hbs
@@ -30,8 +30,7 @@
               {{#each categories as |category|}}
                 {{docs-category
                   category=category
-                  selectCategory=(action "updateSelectedCategories"
-                  tagName="")
+                  selectCategory=(action "updateSelectedCategories" category)
                 }}
               {{/each}}
             </div>
@@ -43,7 +42,7 @@
               {{#each tags as |tag|}}
                 {{docs-tag
                   tag=tag
-                  selectTag=(action "updateSelectedTags")
+                  selectTag=(action "updateSelectedTags" tag)
                 }}
               {{/each}}
             </div>

test/javascripts/acceptance/docs-test.js.es6
@@ -1,4 +1,4 @@
-import { acceptance, queryAll } from "helpers/qunit-helpers";
+import { acceptance, count, query } from "helpers/qunit-helpers";
 import docsFixtures from "../fixtures/docs";
 
 acceptance("Docs", function (needs) {
@@ -8,7 +8,25 @@ acceptance("Docs", function (needs) {
   });
 
   needs.pretender((server, helper) => {
-    server.get("/docs.json", () => helper.response(docsFixtures));
+    server.get("/docs.json", (request) => {
+      if (request.queryParams.category === "1") {
+        const fixture = JSON.parse(JSON.stringify(docsFixtures));
+
+        return helper.response(
+          Object.assign(fixture, {
+            categories: [
+              {
+                id: 1,
+                count: 119,
+                active: true,
+              },
+            ],
+          })
+        );
+      } else {
+        return helper.response(docsFixtures);
+      }
+    });
   });
 
   test("index page", async function (assert) {
@@ -16,11 +34,19 @@ acceptance("Docs", function (needs) {
     await click("#toggle-hamburger-menu");
     await click(".docs-link");
 
-    assert.equal(queryAll(".docs-category")[0].innerText.trim(), "bug 119");
-    assert.equal(queryAll(".docs-tag")[0].innerText.trim(), "something 74");
+    assert.equal(query(".docs-category").innerText.trim(), "bug 119");
+    assert.equal(query(".docs-tag").innerText.trim(), "something 74");
     assert.equal(
-      queryAll(".docs-topic-link")[0].innerText.trim(),
+      query(".docs-topic-link").innerText.trim(),
       "Importing from Software X"
     );
+  });
+
+  test("selecting a category", async function (assert) {
+    await visit("/docs");
+    assert.equal(count(".docs-category.selected"), 0);
+
+    await click(".docs-item.docs-category");
+    assert.equal(count(".docs-category.selected"), 1);
   });
 });