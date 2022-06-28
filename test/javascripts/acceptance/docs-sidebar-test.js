632042981faaaf5aae41608215ed35a39fd70cb1
Jarek Radosz <jradosz@gmail.com>
2022-06-28 22:10:02 +0200

    DEV: Remove `isLegacyEmber` (#100)

--- test/javascripts/acceptance/docs-sidebar-test.js
+++ test/javascripts/acceptance/docs-sidebar-test.js
@@ -1,14 +1,11 @@
+import { test } from "qunit";
 import I18n from "I18n";
-
 import { click, currentURL, visit } from "@ember/test-helpers";
-
 import {
   acceptance,
-  conditionalTest,
   exists,
   query,
 } from "discourse/tests/helpers/qunit-helpers";
-import { isLegacyEmber } from "discourse-common/config/environment";
 import docsFixtures from "../fixtures/docs";
 import { cloneJSON } from "discourse-common/lib/object";
 
@@ -19,18 +16,14 @@ acceptance("Docs - Sidebar with docs disabled", function (needs) {
     docs_enabled: false,
   });
 
-  conditionalTest(
-    "docs sidebar link is hidden",
-    !isLegacyEmber(),
-    async function (assert) {
-      await visit("/");
+  test("docs sidebar link is hidden", async function (assert) {
+    await visit("/");
 
-      assert.ok(
-        !exists(".sidebar-section-link-docs"),
-        "it does not display the docs link in sidebar"
-      );
-    }
-  );
+    assert.ok(
+      !exists(".sidebar-section-link-docs"),
+      "it does not display the docs link in sidebar"
+    );
+  });
 });
 
 acceptance("Docs - Sidebar with docs enabled", function (needs) {
@@ -43,31 +36,23 @@ acceptance("Docs - Sidebar with docs enabled", function (needs) {
     server.get("/docs.json", () => helper.response(cloneJSON(docsFixtures)));
   });
 
-  conditionalTest(
-    "clicking on docs link",
-    !isLegacyEmber(),
-    async function (assert) {
-      await visit("/");
+  test("clicking on docs link", async function (assert) {
+    await visit("/");
 
-      assert.strictEqual(
-        query(".sidebar-section-link-docs").textContent.trim(),
-        I18n.t("sidebar.docs_link_text"),
-        "displays the right text for the link"
-      );
+    assert.strictEqual(
+      query(".sidebar-section-link-docs").textContent.trim(),
+      I18n.t("sidebar.docs_link_text"),
+      "displays the right text for the link"
+    );
 
-      assert.strictEqual(
-        query(".sidebar-section-link-docs").title,
-        I18n.t("sidebar.docs_link_title"),
-        "displays the right title for the link"
-      );
+    assert.strictEqual(
+      query(".sidebar-section-link-docs").title,
+      I18n.t("sidebar.docs_link_title"),
+      "displays the right title for the link"
+    );
 
-      await click(".sidebar-section-link-docs");
+    await click(".sidebar-section-link-docs");
 
-      assert.strictEqual(
-        currentURL(),
-        "/docs",
-        "it navigates to the right page"
-      );
-    }
-  );
+    assert.strictEqual(currentURL(), "/docs", "it navigates to the right page");
+  });
 });
