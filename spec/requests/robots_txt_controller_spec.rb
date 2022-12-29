724100044cd6d7b9593301c2cb978f6fef4ffbdd
David Taylor <david@taylorhq.com>
2022-12-29 12:31:34 +0000

    DEV: Introduce syntax_tree for ruby formatting (#118)

--- spec/requests/robots_txt_controller_spec.rb
+++ spec/requests/robots_txt_controller_spec.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe RobotsTxtController do
   before do
     SiteSetting.docs_enabled = true
-    GlobalSetting.stubs(:docs_path).returns('docs')
+    GlobalSetting.stubs(:docs_path).returns("docs")
   end
 
-  it 'adds /docs/ to robots.txt' do
-    get '/robots.txt'
+  it "adds /docs/ to robots.txt" do
+    get "/robots.txt"
 
-    expect(response.body).to include('User-agent: *')
+    expect(response.body).to include("User-agent: *")
     expect(response.body).to include("Disallow: /#{GlobalSetting.docs_path}/")
   end
 end
