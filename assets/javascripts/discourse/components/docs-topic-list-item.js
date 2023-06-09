005453977550d2fb3cfaf993dcdd67cf13b4a7fe
Jordan Vidrine <jordan@jordanvidrine.com>
2023-06-09 14:08:13 -0500

    REFACTOR: Use the same structure as core does for topic-list-item in docs

---
+++ assets/javascripts/discourse/components/docs-topic-list-item.js
@@ -0,0 +1,24 @@
+import Component from "@ember/component";
+import { RUNTIME_OPTIONS } from "discourse-common/lib/raw-handlebars-helpers";
+import { findRawTemplate } from "discourse-common/lib/raw-templates";
+import { htmlSafe } from "@ember/template";
+
+export default Component.extend({
+  tagName: "tr",
+  classNameBindings: [":topic-list-item"],
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.renderTopicListItem();
+  },
+
+  renderTopicListItem() {
+    const template = findRawTemplate("list/topic-list-item");
+    if (template) {
+      this.set(
+        "topicListItemContents",
+        htmlSafe(template(this, RUNTIME_OPTIONS))
+      );
+    }
+  },
+});
